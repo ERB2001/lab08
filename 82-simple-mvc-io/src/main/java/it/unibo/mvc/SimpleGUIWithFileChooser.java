27438221bb6acc214fc17853c4aee013ef004d1e
Elia Rossi Baroncelli <elia.rossibaroncelli@studio.unibo.it>
2022-11-15 16:24:41 +0100

    End of Exercise 82  -- SimpleGUIFileChooser

--- 82-simple-mvc-io/src/main/java/it/unibo/mvc/SimpleGUIWithFileChooser.java
+++ 82-simple-mvc-io/src/main/java/it/unibo/mvc/SimpleGUIWithFileChooser.java
@@ -1,12 +1,19 @@
 package it.unibo.mvc;
 
 import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.Toolkit;
+import java.io.File;
+import java.io.IOException;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 /**
  * A very simple program using a graphical interface.
@@ -25,6 +32,7 @@ public final class SimpleGUIWithFileChooser {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         JTextField field = new JTextField();
+        field.setEditable(false);
 
         JPanel canvas = new JPanel();
         canvas.setLayout(new BorderLayout());
@@ -39,6 +47,8 @@ public final class SimpleGUIWithFileChooser {
 
         JButton browse = new JButton("Browse...");
 
+        JFileChooser chooser = new JFileChooser();
+
         secondCanvas.add(field, BorderLayout.CENTER);
 
         secondCanvas.add(browse, BorderLayout.LINE_END);
@@ -47,6 +57,49 @@ public final class SimpleGUIWithFileChooser {
 
         frame.add(canvas);
 
+        /*
+         * browse.addActionListener(new ActionListener() {
+         * 
+         * @Override
+         * public void actionPerformed(ActionEvent arg0) {
+         * // TODO Auto-generated method stub
+         * 
+         * try {
+         * if (result == JFileChooser.APPROVE_OPTION) {
+         * Controller controller = new Controller();
+         * File file = new File("SimpleGUIWithFileChooser.txt");
+         * controller.setFile(file);
+         * }
+         * } catch (Exception e) {
+         * // TODO: handle exception
+         * JOptionPane.showMessageDialog(dialog, "Error");
+         * e.printStackTrace();
+         * }
+         * 
+         * }
+         * });
+         */
+
+        browse.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent arg0) {
+                // TODO Auto-generated method stub
+                int result = chooser.showSaveDialog(frame);
+
+                JDialog dialog = new JDialog();
+
+                if (result == JFileChooser.APPROVE_OPTION) {
+                    Controller controller = new Controller();
+                    // File file = new File("output.txt");
+                    controller.setFile(chooser.getSelectedFile());
+                    field.setText(controller.getPath());
+                    field.getText();
+                } else {
+                    JOptionPane.showMessageDialog(dialog, "Error");
+                }
+            }
+        });
     }
 
     public void display() {
