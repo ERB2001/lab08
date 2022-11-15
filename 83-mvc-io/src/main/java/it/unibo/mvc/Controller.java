10fcb8a04ee2da21ad2e449cd6e60b2858fe8308
Elia Rossi Baroncelli <elia.rossibaroncelli@studio.unibo.it>
2022-11-15 21:43:54 +0100

    First Implementation of Interface of Exercise 83

--- 83-mvc-io/src/main/java/it/unibo/mvc/Controller.java
+++ 83-mvc-io/src/main/java/it/unibo/mvc/Controller.java
@@ -1,8 +1,18 @@
 package it.unibo.mvc;
 
+import java.util.List;
+
 /**
  *
  */
 public interface Controller {
+
+    void setNextString(String s);
+
+    String getNextString();
+
+    List<String> getPrintedElementHistory();
+
+    String toString();
 
 }
