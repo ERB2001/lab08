ba27b8139ec5fe1a091a40c59c532e754fa92bf2
Elia Rossi Baroncelli <elia.rossibaroncelli@studio.unibo.it>
2022-11-15 14:05:47 +0100

    re-finish (Exercise 82)

--- 82-simple-mvc-io/src/main/java/it/unibo/mvc/Controller.java
+++ 82-simple-mvc-io/src/main/java/it/unibo/mvc/Controller.java
@@ -32,7 +32,7 @@ public class Controller {
 
     public void saveInFile(String s) {
 
-        try (PrintStream ps = new PrintStream(fileName, StandardCharsets.UTF_8)) {
+        try (PrintStream ps = new PrintStream(FILENAME, StandardCharsets.UTF_8)) {
             ps.append(s);
         } catch (IOException e1) {
             e1.printStackTrace(); // NOPMD: allowed as this is just an exercise
