026078777ca66217c08faa55318c65b623b0c9eb
Elia Rossi Baroncelli <elia.rossibaroncelli@studio.unibo.it>
2022-11-16 17:54:19 +0100

    Implementation of the first elements of the window such as: the 2 buttons at the bottom, the textarea in the center and the textfield at the top(part 2 of Exercise 83)

--- 83-mvc-io/src/main/java/it/unibo/mvc/SimpleGUI.java
+++ 83-mvc-io/src/main/java/it/unibo/mvc/SimpleGUI.java
@@ -1,6 +1,14 @@
 package it.unibo.mvc;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+
+import java.awt.Dimension;
+import java.awt.Toolkit;
+import java.awt.BorderLayout;
 
 /**
  * A very simple program using a graphical interface.
@@ -8,6 +16,58 @@ import javax.swing.JFrame;
  */
 public final class SimpleGUI {
 
+    private static final int PROPORTION = 5;
+
+    private static final String TITLE = "SimpleGUI";
+
     private final JFrame frame = new JFrame();
+
+    public SimpleGUI() {
+        frame.setTitle(TITLE);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JPanel canvas = new JPanel();
+        canvas.setLayout(new BorderLayout());
+
+        JTextField textField = new JTextField();
+
+        canvas.add(textField, BorderLayout.NORTH);
+
+        JTextArea textArea = new JTextArea();
+
+        canvas.add(textArea, BorderLayout.CENTER);
+
+        JButton print = new JButton("Print");
+
+        JPanel secondCanvas = new JPanel();
+
+        JButton showHistory = new JButton("Show History");
+
+        secondCanvas.add(showHistory, BorderLayout.WEST);
+
+        secondCanvas.add(print, BorderLayout.EAST);
+
+        canvas.add(secondCanvas, BorderLayout.SOUTH);
+
+        frame.add(canvas);
+
+    }
+
+    public void display() {
+        final Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
+        final int sw = (int) screen.getWidth();
+        final int sh = (int) screen.getHeight();
+
+        frame.setSize(sw / PROPORTION, sh / PROPORTION);
+
+        frame.setLocationByPlatform(true);
+
+        frame.setVisible(true);
+
+    }
+
+    public static void main(String[] args) {
+        new SimpleGUI().display();
+    }
 
 }
