b65e0a23e9341cd324d53608aff35b2b29dfd504
Elia Rossi Baroncelli <elia.rossibaroncelli@studio.unibo.it>
2022-11-17 16:54:12 +0100

    good implementation of the button showHistory (Exercise 83)

--- 83-mvc-io/src/main/java/it/unibo/mvc/SimpleGUI.java
+++ 83-mvc-io/src/main/java/it/unibo/mvc/SimpleGUI.java
@@ -32,7 +32,7 @@ public final class SimpleGUI {
         canvas.setLayout(new BorderLayout());
 
         JTextField textField = new JTextField();
-        textField.setEditable(false);
+        textField.setEditable(true);
 
         canvas.add(textField, BorderLayout.NORTH);
 
@@ -54,16 +54,31 @@ public final class SimpleGUI {
 
         frame.add(canvas);
 
+        SimpleController controller = new SimpleController();
+
         print.addActionListener(new ActionListener() {
 
             @Override
             public void actionPerformed(ActionEvent arg0) {
                 // TODO Auto-generated method stub
-                SimpleController controller = new SimpleController();
-                controller.setNextString(textArea.getText());
+                controller.setNextString(textField.getText());
                 controller.printString();
-                textField.setText(controller.getNextString());
-                textField.getText();
+            }
+        });
+
+        showHistory.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent arg0) {
+                // TODO Auto-generated method stub
+                String myString = "";
+                for (String s : controller.getPrintedElementHistory()) {
+                    textArea.setText(myString += s + "\n");
+                }
+                // textArea.setText(String.valueOf(controller.getPrintedElementHistory()));
+
+                // textArea.setText(controller.getPrintedElementHistory().toString());
+
             }
         });
 
@@ -85,5 +100,9 @@ public final class SimpleGUI {
     public static void main(String[] args) {
         new SimpleGUI().display();
     }
+    /*
+     * usare il getText() quando si vuole prendere il testo
+     * e non stamparlo
+     */
 
 }
